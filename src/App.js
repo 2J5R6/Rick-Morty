2c82ef12475935603425ed6f56857d98550b4117
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-05-03 23:39:22 -0500

    Unicorn Vomit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 // import Card from './components/Card/Card.jsx'
 import Cards from './components/Cards/Cards.jsx'
 import SearchBar from './components/SearchBar/SearchBar.jsx'
-import characters, { Rick } from './data.js'
+import characters /*, { Rick }*/ from './data.js'
 
 function App () {
   return (
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,18 @@
+import React from "react"
 import styles from "./Card.module.css";
 // export default function Card({id,name,species,gender,image,onClose}) {
 export default function Card(props) {
+   const [selected, setSelected] = React.useState(false);
+
+  const handleClick = () => {
+    setSelected(!selected);
+  };
    
    return (
-      <div className={styles.DivCard} key={props.id}>
-         <button onClick={props.onClose}>X</button>
+      <div className={`${styles.DivCard} ${selected ? styles.selected : ""}`}
+      key={props.id}
+      onClick={handleClick}>
+         <button className={styles.Button} href="#" data-back="C-137" data-front="To another dimention" onClick={props.onClose}/>
          <h2>{props.name}</h2>
          <h2>{props.status}</h2>
          <h2>{props.species}</h2>
